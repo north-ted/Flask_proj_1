42db072ec48f612b69902d62e292db720daf78f6
north-ted <thejasd2001@gmail.com>
2023-11-25 18:25:24 +0530

    Added about page

--- app.py
+++ app.py
@@ -6,5 +6,9 @@ app=Flask(__name__)
 def hello():
     return "<h1>Hello World!!!</h1>"
 
+@app.route("/about")
+def about():
+    return "<h1>About</h1>"
+
 if __name__=="__main__":
     app.run(debug=True)
